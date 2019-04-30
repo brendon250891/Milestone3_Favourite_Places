b7522bf692f585b2213d6417fd95577f6e21b577
Brendon Willoughby <Brendon.willoughby@gmail.com>
2019-05-01 09:55:40 +1000

    Beginning of Milestone3 implementation

---
+++ Milestone3_FavouritePlaces/Model/SunriseAndSunset.swift
@@ -0,0 +1,26 @@
+//
+//  SunriseAndSunset.swift
+//  Milestone3_FavouritePlaces
+//
+//  Created by Brendon on 1/5/19.
+//  Copyright © 2019 Brendon. All rights reserved.
+//
+
+import Foundation
+
+struct SunriseAndSunset: Codable {
+    /// The sunrise time.
+    var sunrise: String
+    
+    /// The sunset time.
+    var sunset: String
+    
+    /// The length of the day.
+    var dayLength: String
+    
+    /// The
+    var midday: String
+    
+    /// The twilight time.
+    var twilight: String
+}
