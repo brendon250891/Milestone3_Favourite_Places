0fe43dbf1ad804f913f02c79e06480f1ef1dee8f
Brendon Willoughby <Brendon.willoughby@gmail.com>
2019-05-01 08:15:05 +1000

    Initial Commit

---
+++ Milestone3_FavouritePlacesUITests/Milestone3_FavouritePlacesUITests.swift
@@ -0,0 +1,34 @@
+//
+//  Milestone3_FavouritePlacesUITests.swift
+//  Milestone3_FavouritePlacesUITests
+//
+//  Created by Brendon on 1/5/19.
+//  Copyright © 2019 Brendon. All rights reserved.
+//
+
+import XCTest
+
+class Milestone3_FavouritePlacesUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
